47dd5204f4dd96568068ac1c079dfb66b30a6aa1
Gaurav Gupta <ggupta2005@gmail.com>
2016-03-24 00:04:54 -0700

    new: dev: Adding the IPv6 static route configuration generation code
    Committing ipv4_route_gen.py to network tools and utilities repo.
    This file contains the logic to generate IPv4 and IPv6 static routes
    configuration for a given number of routes and specified nexthops.
    
    Signed-off-by: Gaurav Gupta <ggupta2005@gmail.com>

--- ipv4_route_gen.py
+++ ipv4_route_gen.py
@@ -1,11 +1,46 @@
 import sys
 import socket
 
-def get_static_ipv4_route_by_count(count, nexthop_list):
+IPV4_MIN_OCTET_NUMBER = 1
+IPV4_MAX_OCTET_NUMBER = 200
+IPV6_MIN_OCTET_NUMBER = 1
+IPV6_MAX_OCTET_NUMBER = 9999
+
+
+def get_ipv4_static_route_config_string(**kwargs):
+    first_octet = str(kwargs.get('first_octet', None))
+    second_octet = str(kwargs.get('second_octet', None))
+    third_octet = str(kwargs.get('third_octet', None))
+    fourth_octet = str(kwargs.get('fourth_octet', None))
+    if_ipv4 = str(kwargs.get('if_ipv4', False))
+
+    nexthop = str(kwargs.get('nexthop', None))
+
+    if first_octet is None or second_octet is None or \
+       third_octet is None or fourth_octet is None or \
+       nexthop is None:
+        return None
+
+    if if_ipv4 is True:
+        route_string = 'ip route %s.%s.%s.%s/%s %s' %(first_octet,
+                       second_octet, third_octet, fourth_octet , '32',
+                       nexthop)
+    else:
+        route_string = 'ipv6 route %s:%s:%s:%s::1/%s %s' %(first_octet,
+                       second_octet, third_octet, fourth_octet , '128',
+                       nexthop)
+
+    return route_string
+
+
+def get_static_ipv4_route_by_count(if_ipv4, count, nexthop_list):
 
     route_count = int(count)
 
     if route_count <= 0:
+        return
+
+    if if_ipv4 is not True and if_ipv4 is not False:
         return
 
     if nexthop_list is None:
@@ -14,19 +49,34 @@ def get_static_ipv4_route_by_count(count, nexthop_list):
     if len(nexthop_list) == 0:
         return
 
+    min_number = IPV4_MIN_OCTET_NUMBER
+    max_number = IPV4_MAX_OCTET_NUMBER + 1
+
+    if if_ipv4 is False:
+        min_number = IPV6_MIN_OCTET_NUMBER
+        max_number = IPV6_MAX_OCTET_NUMBER + 1
+
     total_routes = 0
-    for first_octet in range(1, 200):
-        for second_octet in range(0, 200):
-            for third_octet in range(0, 200):
-                for fourth_octet in range(0, 200):
+    for first_octet in range(min_number, max_number):
+        for second_octet in range(min_number, max_number):
+            for third_octet in range(min_number, max_number):
+                for fourth_octet in range(min_number, max_number):
 
                     if total_routes >= route_count:
                         break
 
                     for nexthop_index in range(0, len(nexthop_list)):
-                        route_string = 'ip route %s.%s.%s.%s/%s %s' %(str(first_octet),
-                                       str(second_octet), str(third_octet), str(fourth_octet) , '32',
-                                       nexthop_list[nexthop_index])
+
+                        route_string = get_ipv4_static_route_config_string(
+                                            first_octet=first_octet,
+                                            second_octet=second_octet,
+                                            third_octet=third_octet,
+                                            fourth_octet=fourth_octet,
+                                            if_ipv4=if_ipv4,
+                                            nexthop=nexthop_list[nexthop_index])
+
+                        if route_string is None:
+                            return
 
                         print route_string
 
@@ -53,6 +103,39 @@ def validate_route_count(count):
         return False
 
     return True
+
+
+def validate_address_family(address_family):
+    if address_family is None:
+        return False
+
+    if address_family.lower() == "IPv4".lower():
+        return True
+
+    if address_family.lower() == "IPv6".lower():
+        return True
+
+    return False
+
+
+def if_address_family_is_ipv4(address_family):
+    if address_family is None:
+        return False
+
+    if address_family.lower() == "IPv4".lower():
+        return True
+
+    return False
+
+
+def if_address_family_is_ipv6(address_family):
+    if address_family is None:
+        return False
+
+    if address_family.lower() == "IPv6".lower():
+        return True
+
+    return False
 
 
 def validate_interface_name(interface_name):
@@ -90,7 +173,7 @@ if __name__ == '__main__':
 
     if len(sys.argv) < 3:
 
-        print "Usage: python ipv4_route_gen.py <number of routes> <nexthop1> <nexthop2>..."
+        print "Usage: python ipv4_route_gen.py <number of routes> <address-family> <nexthop1> <nexthop2>..."
 
     else:
 
@@ -98,17 +181,20 @@ if __name__ == '__main__':
 
         if validate_route_count(sys.argv[1]) is False:
             assert False, "Invalid value of number of routes!!!"
-            #return
 
         route_count = sys.argv[1]
 
-        for nexthop_index in range(2, (len(sys.argv))):
+        if validate_address_family(sys.argv[2]) is False:
+            assert False, "Invalid address family!!!"
+
+        if_ipv4 = if_address_family_is_ipv4(sys.argv[2])
+
+        for nexthop_index in range(3, (len(sys.argv))):
             if validate_nexthop(sys.argv[nexthop_index]) is False:
                 assert False, "Invalid nexthop " + sys.argv[nexthop_index] + "!!!"
-                #return
 
             nexthop_list.append(sys.argv[nexthop_index])
 
         print nexthop_list
-        get_static_ipv4_route_by_count(route_count, nexthop_list)
+        get_static_ipv4_route_by_count(if_ipv4, route_count, nexthop_list)
 
